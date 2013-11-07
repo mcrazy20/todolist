00506e893ac9e8b485dfd44ce6e97c26d5b36cc2
Jeff Kikel <mcrazy300@yahoo.com>
2013-11-07 11:41:46 -0500

    Revert "Working on model?"
    This reverts commit b578d84050fc012691c1bf787f25fa43bb7ea149.

--- app/models/task_list.rb
+++
@@ -1,4 +0,0 @@
-class TaskList < ActiveRecord::Base
-  attr_accessible :has_many
-  has_many :tasks
-end
--- db/migrate/20131107163211_create_task_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateTaskLists < ActiveRecord::Migration
-  def change
-    create_table :task_lists do |t|
-      t.tasks :has_many
-
-      t.timestamps
-    end
-  end
-end
